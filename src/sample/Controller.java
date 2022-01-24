887d3b64ce7d9861c7e98274a6a30f140b8320c5
AlekseyBogdanov <bogdanov.lexa1234@yandex.ru>
2022-01-24 23:21:37 +0300

    Home work Java2 lesson4

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,30 @@
 package sample;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 public class Controller {
+    @FXML
+    public TextField textField;
+    @FXML
+    public TextArea textArea;
+
+    @FXML
+    public void sendText (ActionEvent actionEvent) {
+        textArea.appendText(textField.getText() + "\n");
+        textField.clear();
+        textField.requestFocus();
+
+    }
+    @FXML
+    public void exitAction(ActionEvent actionEvent) {
+        Platform.runLater(() -> {
+            Stage stage = (Stage) textField.getScene().getWindow();
+            stage.close();
+        });
+    }
 }
